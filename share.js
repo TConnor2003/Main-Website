fcd620b7a92b107f420c4f76ad2c31af6c5f51b1
Crafty Playz <86719941+CraftyPlayz@users.noreply.github.com>
2022-12-09 14:27:18 +0000

    change to circle following mouse

--- share.js
+++ share.js
@@ -1,6 +1,6 @@
 document.addEventListener('mousemove', function(e) {
     let body = document.querySelector('body');
-    let circle = document.getElementById('share');
+    let circle = document.getElementById('circle');
     let left = e.offsetX;
     let top = e.offsetY;
     circle.style.left = left + 'px';
