49d815d3064d3f5d63c15fa8aca6649f8c24c015
Crafty Playz <86719941+CraftyPlayz@users.noreply.github.com>
2022-12-09 14:38:18 +0000

    rick roll instead of text

--- share.js
+++
@@ -1,6 +0,0 @@
-let circle = document.getElementById('circle');
-const onMouseMove = (e) =>{
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
-}
-document.addEventListener('mousemove', onMouseMove);
