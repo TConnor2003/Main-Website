a83c0df265c2bde481162b37cad9df3e3d339b43
Crafty Playz <86719941+CraftyPlayz@users.noreply.github.com>
2022-12-09 14:28:01 +0000

    fix laggy circle moving after mouse

--- share.js
+++ share.js
@@ -1,8 +1,6 @@
-document.addEventListener('mousemove', function(e) {
-    let body = document.querySelector('body');
-    let circle = document.getElementById('circle');
-    let left = e.offsetX;
-    let top = e.offsetY;
-    circle.style.left = left + 'px';
-    circle.style.top = top + 'px';
-  });
+let circle = document.getElementById('circle');
+const onMouseMove = (e) =>{
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+}
+document.addEventListener('mousemove', onMouseMove);
